105b5784f3882b2abbb843fc122bcc1225be8b74
Brennan Falkner <Brennan@bfalkner.com>
2015-03-22 19:40:49 -0400

    oops

--- index.js
+++ index.js
@@ -1,24 +1,29 @@
 /* jshint node: true */
 'use strict';
 
-var pickFiles = require('broccoli-static-compiler'),
-    jsFileName = this.app.env === 'production' ? 'gss.min.js' : 'gss.js';
+var pickFiles = require('broccoli-static-compiler');
 
 module.exports = {
   name: 'ember-cli-gss',
+
+  included: function(app) {
+    this._super.included(app);
+
+    this.jsFileName = this.app.env === 'production' ? 'gss.min.js' : 'gss.js';
+  },
 
   contentFor: function(type, config) {
     if (type === 'head') {
       return '<link rel="stylesheet" type="text/gss" href="assets/app.gss">';
     } else if (type === 'body-footer') {
-      return '<script src="assets/' + jsFileName + '"></script>';
+      return '<script src="assets/' + this.jsFileName + '"></script>';
     }
   },
 
   treeForPublic: function() {
     return pickFiles(this.app.bowerDirectory, {
       srcDir: '/gss/dist',
-      files: [jsFileName],
+      files: [this.jsFileName],
       destDir: '/assets'
     });
   }
